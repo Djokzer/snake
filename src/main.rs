1870abd83d976637341ece0d56eaba5173b3c448
Abivarman <abivarman.kandiah@etu.hesge.ch>
2022-04-29 12:47:46 +0200

    Add raylib lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use raylib::prelude::*;
+
+fn main() 
+{
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Snake")
+        .build();
+
+    while !rl.window_should_close() 
+    {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+        d.draw_text("SNAKE GAME!", 12, 12, 20, Color::WHITE);
+    }
 }
